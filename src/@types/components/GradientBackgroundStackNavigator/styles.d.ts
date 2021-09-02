45f32632acf7d570d07b7fc2727e7ea44383ad21
Victor Batalha <madivcb@gmail.com>
2021-09-01 23:51:50 -0300

    chore: moved types to type definition file

--- src/@types/components/GradientBackgroundStackNavigator/styles.d.ts
+++ src/@types/components/GradientBackgroundStackNavigator/styles.d.ts
@@ -1,12 +1,19 @@
-declare namespace GradientBackgroundStackNavigatorNamespace {
+/// <reference types="react" />
+
+declare namespace GradientBackgroundStackNavigatorNamespace.Styles {
+  type PropsWithChildren<P> = React.PropsWithChildren<P>;
+
   type LinearGradientProps =
     import('react-native-linear-gradient').LinearGradientProps;
-
   interface ContainerProps {
     backgroundColors?: (string | number)[];
   }
 
-  export type CustomContainerProps = Partial<LinearGradientProps> &
-    ContainerProps &
-    Omit<GradientBackgroundStackNavigatorNamespace.CustomProps, 'colors'>;
+  export type CustomContainerProps = (
+    props: PropsWithChildren<
+      Partial<LinearGradientProps> &
+        ContainerProps &
+        Omit<GradientBackgroundStackNavigatorNamespace.CustomProps, 'colors'>
+    >
+  ) => any;
 }
--- src/components/GradientBackgroundStackNavigator/styles.ts
+++ src/components/GradientBackgroundStackNavigator/styles.ts
@@ -4,21 +4,18 @@ import LinearGradient, {
   LinearGradientProps,
 } from 'react-native-linear-gradient';
 
-export const Container = styled<
-  React.ComponentType<GradientBackgroundStackNavigatorNamespace.CustomContainerProps>
->(
-  LinearGradient as unknown as (
-    props: React.PropsWithChildren<GradientBackgroundStackNavigatorNamespace.CustomContainerProps>,
-    context?: any
-  ) => any
-).attrs<GradientBackgroundStackNavigatorNamespace.ContainerProps>(props => {
-  const { primary, lightPrimary } = props.theme.colors;
+export const Container = styled<GradientBackgroundStackNavigatorNamespace.Styles.CustomContainerProps>(
+  LinearGradient as unknown as GradientBackgroundStackNavigatorNamespace.Styles.CustomContainerProps
+).attrs<GradientBackgroundStackNavigatorNamespace.Styles.ContainerProps>(
+  props => {
+    const { primary, lightPrimary } = props.theme.colors;
 
-  return {
-    colors: props.backgroundColors || [primary, lightPrimary],
-    start: props.start || { x: 0, y: 0.45 },
-    end: props.end || { x: 1, y: 0.6 },
-  } as LinearGradientProps;
-})`
+    return {
+      colors: props.backgroundColors || [primary, lightPrimary],
+      start: props.start || { x: 0, y: 0.45 },
+      end: props.end || { x: 1, y: 0.6 },
+    } as LinearGradientProps;
+  }
+)`
   flex: 1;
 `;
