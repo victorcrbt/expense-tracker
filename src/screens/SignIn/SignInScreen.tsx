a9f9d9f27f6d4bd019bb1406569303863bdc87dc
Victor Batalha <madivcb@gmail.com>
2021-09-05 15:04:09 -0300

    feat: created sign in screen structure

--- src/screens/SignIn/SignInScreen.tsx
+++ src/screens/SignIn/SignInScreen.tsx
@@ -1,16 +1,36 @@
 import React from 'react';
 import { StackScreenProps } from '@react-navigation/stack';
-import { Text } from 'react-native';
 
+// Shared Components
+import { Typography } from '@components/Typography';
 import { Button } from '@components/Button';
+import { TextInput } from '@components/TextInput';
+import { Link } from '@components/Link';
+
+// Local Imports
+import { Container } from './styles';
 
 export const SignInScreen: React.FC<
   StackScreenProps<GuestNavigatorNamespace.ParamsList, 'SignIn'>
-> = ({ navigation }) => (
-  <>
-    <Text>Sing In</Text>
-    <Button onPress={() => navigation.navigate('SingUp')}>
-      Navegar para o cadastro
-    </Button>
-  </>
-);
+> = ({ navigation }) => {
+  function navigateToSignUpScreen() {
+    navigation.navigate('SingUp');
+  }
+
+  return (
+    <Container>
+      <Typography variant="h1">Login</Typography>
+
+      <TextInput label="E-mail" placeholder="Digite seu e-mail..." />
+      <TextInput label="Senha" placeholder="Digite sua senha..." />
+
+      <Button onPress={() => {}} marginTop={16}>
+        entrar
+      </Button>
+
+      <Link onPress={navigateToSignUpScreen}>
+        Não possui cadastro? Faça agora!
+      </Link>
+    </Container>
+  );
+};
