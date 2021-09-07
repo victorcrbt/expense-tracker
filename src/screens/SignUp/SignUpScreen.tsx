cb76bd3cdd7723e57ed120667198864f483b0e2f
Victor Batalha <madivcb@gmail.com>
2021-09-07 09:43:25 -0300

    feat: created sign up screen structure

--- src/screens/SignUp/SignUpScreen.tsx
+++ src/screens/SignUp/SignUpScreen.tsx
@@ -2,15 +2,37 @@ import React from 'react';
 import { StackScreenProps } from '@react-navigation/stack';
 import { Text, Pressable } from 'react-native';
 
+// Shared Components
+import { Typography } from '@components/Typography';
 import { Button } from '@components/Button';
+import { TextInput } from '@components/TextInput';
+import { Link } from '@components/Link';
+
+// Local Imports
+import { Container } from './styles';
 
 export const SingUpScreen: React.FC<
   StackScreenProps<GuestNavigatorNamespace.ParamsList, 'SingUp'>
-> = ({ navigation }) => (
-  <>
-    <Text>Sign Up</Text>
-    <Button onPress={() => navigation.navigate('SignIn')}>
-      Navegar para o login
-    </Button>
-  </>
-);
+> = ({ navigation }) => {
+  function navigateToSignInScreen() {
+    navigation.navigate('SignIn');
+  }
+
+  return (
+    <Container>
+      <Typography variant="h1">Cadastro</Typography>
+
+      <TextInput label="Nome" placeholder="Digite seu nome..." />
+      <TextInput label="E-mail" placeholder="Digite seu e-mail..." />
+      <TextInput label="Senha" placeholder="Digite sua senha..." />
+
+      <Button onPress={() => {}} marginTop={16}>
+        cadastrar
+      </Button>
+
+      <Link onPress={navigateToSignInScreen}>
+        Já possui cadastro? Entre agora!
+      </Link>
+    </Container>
+  );
+};
---
+++ src/screens/SignUp/styles.ts
@@ -0,0 +1,9 @@
+import styled from 'styled-components/native';
+
+export const Container = styled.KeyboardAvoidingView`
+  width: 100%;
+
+  flex: 1;
+  justify-content: center;
+  align-items: center;
+`;
