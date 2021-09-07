74448329e7cf182577aa24ded70ab289bddeb7c1
Victor Batalha <madivcb@gmail.com>
2021-09-07 11:09:35 -0300

    feat: add ref to custom input

--- src/components/TextInput/TextInput.tsx
+++ src/components/TextInput/TextInput.tsx
@@ -1,27 +1,33 @@
-import React from 'react';
+import React, { forwardRef } from 'react';
+import { TextInput as RNTextInput } from 'react-native';
 
 import { Container, LabelWrapper, LabelText, Input } from './styles';
 
-export const TextInput: React.FC<TextInputNamespace.Props> = ({
-  label,
-  marginTop = '0',
-  marginRight = '0',
-  marginBottom = '16',
-  marginLeft = '0',
-  ...props
-}) => (
-  <Container
-    marginTop={marginTop}
-    marginRight={marginRight}
-    marginBottom={marginBottom}
-    marginLeft={marginLeft}
-  >
-    {label && (
-      <LabelWrapper>
-        <LabelText>{label}</LabelText>
-      </LabelWrapper>
-    )}
+export const TextInput = forwardRef<RNTextInput, TextInputNamespace.Props>(
+  (
+    {
+      label,
+      marginTop = '0',
+      marginRight = '0',
+      marginBottom = '16',
+      marginLeft = '0',
+      ...props
+    },
+    ref
+  ) => (
+    <Container
+      marginTop={marginTop}
+      marginRight={marginRight}
+      marginBottom={marginBottom}
+      marginLeft={marginLeft}
+    >
+      {label && (
+        <LabelWrapper>
+          <LabelText>{label}</LabelText>
+        </LabelWrapper>
+      )}
 
-    <Input {...props} placeholderTextColor="#e5e5e5" />
-  </Container>
+      <Input ref={ref} {...props} placeholderTextColor="#e5e5e5" />
+    </Container>
+  )
 );
